e0268354af99a003f2673f48028a1cf9ac3b0ee5
Rjay <rjayavancena7@gmail.com>
2025-12-11 21:19:00 +0800

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,21 @@
+<?php
+// config/db.php
+class Database {
+    private $host = 'localhost';
+    private $db_name = 'LIBRARY_SYSTEM';
+    private $username = 'root';
+    private $password = '';
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
